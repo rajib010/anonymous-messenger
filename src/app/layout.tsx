a0f0d6c74fef2b33f528f4117b9ab9a0e75fdb8f
raJiB <pokhrelrajib016@gmail.com>
2024-10-21 09:56:18 +0545

    added the message card component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '../globals.css';
-import Navbar from '@/components/Navbar';
+import './globals.css';
+import AuthProvider from '@/context/AuthProvider';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,10 +18,12 @@ interface RootLayoutProps {
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" >
+      <AuthProvider>
         <body className={inter.className}>
-          <Navbar/>
           {children}
+          <Toaster />
         </body>
+      </AuthProvider>
     </html>
   );
 }
